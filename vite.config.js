61a295e81d5e81c1102da2fa4ab818b689fc6eb7
KampeGen2020 <oscar.rodriguez.fuertes@gmail.com>
2026-08-09 00:04:45 -0400

    GIt fix

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
 import { defineConfig } from 'vite';
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1];
+const githubPagesBase = repositoryName
+  ? repositoryName.endsWith('.github.io')
+    ? '/'
+    : `/${repositoryName}/`
+  : '/';
+
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS === 'true' ? githubPagesBase : '/',
   server: { port: 5173 },
 });
